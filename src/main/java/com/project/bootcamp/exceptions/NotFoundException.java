d9eec26b7507e60eb66d228e6be3c214016e5d5e
edvaldoljr <edvaldoluizj@gmail.com>
2021-06-24 16:36:18 -0300

    Criação de repositório com o código do bootcamp

---
+++ src/main/java/com/project/bootcamp/exceptions/NotFoundException.java
@@ -0,0 +1,11 @@
+package com.project.bootcamp.exceptions;
+
+import com.project.bootcamp.util.MessageUtil;
+
+
+public class NotFoundException extends RuntimeException {
+
+    public NotFoundException(){
+        super(MessageUtil.NO_RECORDS_FOUND);
+    }
+}
